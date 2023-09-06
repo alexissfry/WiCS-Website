a8e3a8dfffd17ca6fbaebf938dc0c67bfd97e890
harukam122 <mashar0122.yhm@gmail.com>
2023-09-06 19:16:19 -0400

    feat: all photos in

--- wics-app/src/assets/portraits/portraits.js
+++ wics-app/src/assets/portraits/portraits.js
@@ -23,7 +23,7 @@ export const profiles = [
   {
     name: "Alexis Fry '25",
     title: "Technology Co-Chair",
-    image: "eye.jpg",
+    image: "afry.jpg",
   },
   {
     name: "Haruka Masamura '25",
@@ -31,23 +31,23 @@ export const profiles = [
     image: "hmasamura.jpg",
   },
   {
-    name: "Jo Kavishe '25",
-    title: "",
-    image: "eye.jpg",
-  },
-  {
-    name: "Maria Chavez Zapata '25",
-    title: "",
-    image: "eye.jpg",
-  },
-  {
-    name: "Dior Williams '26",
-    title: "",
-    image: "dwilliams.jpg",
+    name: "Fernanda Chavez '25",
+    title: "Mentorship Co-Chair",
+    image: "fchavez.jpg",
   },
   {
     name: "Rebecca Chou '26",
-    title: "",
+    title: "Mentorship Co-Chair",
     image: "rchou.jpg",
+  },
+  {
+    name: "Dior Williams '26",
+    title: "Marketing Co-Chair",
+    image: "dwilliams.jpg",
+  },
+  {
+    name: "Jo Kavishe '25",
+    title: "Marketing Co-Chair",
+    image: "jkavishe.jpg",
   },
 ];
